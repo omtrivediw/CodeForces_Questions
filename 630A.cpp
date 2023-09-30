017a936ae6cc2ac4fd4b2ce7a7244974eb175e02
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-09-30 11:16:09 +0530

    Create 630A.cpp

---
+++ 630A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define py cout<<YES<<endl;
+#define pn cout<<NO<<endl;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << 25 << endl;
+}
+int main()
+{
+    solve();
+}
